b36417e95e4a3c19a52e6e97c930d78d6c69ea4c
lazyboytql <megusta.hz910@gmail.com>
2024-09-17 09:37:57 +0700

    File config

--- app/config.py
+++ app/config.py
@@ -1,14 +1,16 @@
-YOUTUBE_API_KEY = 'AIzaSyBAnDCX6u1ya5cHQQzN9rQdn43ECzr3pvY'
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
+YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY')
 DB_CONFIG = {
-    'host': 'host.docker.internal',
-    'database': 'postgres',
-    'user': 'postgres',
-    'password': 'admin',
-    'port':'5432'
+    'host': os.getenv('DB_HOST'),
+    'database': os.getenv('DB_NAME'),
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASSWORD'),
+    'port': os.getenv('DB_PORT')
 }
 
-CHANNEL_ID = 'UCQ0jSGgYMLmRMeTE6UaPPXg'
-
-MAX_VIDEOS_PER_CHANNEL = 10
-MAX_COMMENTS_PER_VIDEO = 50
+CHANNEL_ID = os.getenv('CHANNEL_ID')
+MAX_VIDEOS_PER_CHANNEL = int(os.getenv('MAX_VIDEOS_PER_CHANNEL'))
+MAX_COMMENTS_PER_VIDEO = int(os.getenv('MAX_COMMENTS_PER_VIDEO'))
